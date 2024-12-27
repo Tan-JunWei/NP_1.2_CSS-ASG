15c58240e618d762d34b3fa4d3fd1a27f67c7532
jwtan <jwtan0402@gmail.com>
2024-12-27 22:19:04 +0800

    feat: add basic navbar and example pages

---
+++ app/UI/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Link className={styles.navitem} href="/">Home</Link>
+      <Link className={styles.navitem} href="/history">History</Link>
+      <Link className={styles.navitem} href="/about">About</Link>
+      <Link className={styles.navitem} href="/contact">Contact</Link>
+    </nav>
+  );
+}
