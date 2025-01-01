7a063dce9bcca6eed0493652f9599debe5f40ffa
Ryan Tan <jjryantan@gmail.com>
2025-01-01 16:00:37 +0800

    feat: add text, image and updated the global css.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,24 @@
+import styles from '@/src/styles/pages/about.module.css'
+import Image from 'next/image';
+import { StaticCopyUsage } from 'three';
+
 export default function About() {
     return (
         <div>
-            <h1>About</h1>
+            <h1 className={styles.header}>About</h1>
+            <p className={styles.main}>
+                Hi! We're a bunch if foodie who really love food and the wonderful world of the internet.
+            </p>
+            <Image 
+            id={styles.mainimage}
+            src="/about/Placeholder.png"
+            width={200}
+            height={150}
+            alt="PLACEHOLDER"/>
+            <p className={styles.p}>
+                We created this website because of our love for singapore food and history behind it.
+                Therefore, we created this website to share our love of food.
+            </p>
         </div>
     );
 }
