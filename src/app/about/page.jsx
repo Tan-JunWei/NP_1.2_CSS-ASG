79cf54b3f7a213fd8a63f0921724ed7076b7c35d
Ryan Tan <jjryantan@gmail.com>
2025-01-02 15:05:49 +0800

    feat: add a type new of carousel, rewrite about page.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,17 @@
+// About page, made by Ryan Tan :D
+
+import ControlledCarousel from '@/src/components/image carousel/about_carousel';
 import styles from '@/src/styles/pages/about.module.css'
 import Image from 'next/image';
 import { StaticCopyUsage } from 'three';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function About() {
     return (
         <div>
             <h1 className={styles.header}>About</h1>
             <p className={styles.main}>
-                Hi! We're a bunch if foodie who really love food and the wonderful world of the internet.
+            Discover the Flavors of Singapore!
             </p>
             <Image 
             id={styles.mainimage}
@@ -16,9 +20,14 @@ export default function About() {
             height={150}
             alt="PLACEHOLDER"/>
             <p className={styles.p}>
-                We created this website because of our love for singapore food and history behind it.
-                Therefore, we created this website to share our love of food.
+            Welcome to Savor & Spice, a food blog dedicated to celebrating the joy of cooking, eating, and exploring diverse cuisines. 
+            Whether you're a seasoned chef or a kitchen newbie, you'll find inspiration in our collection of flavorful recipes, cooking tips, and food stories from around the world. 
+            Join us as we share the art of creating delicious dishes and the memories they inspire. 
+            Let’s make every meal an adventure!
             </p>
+
+            <h2 className={styles.main}>Meet the team!</h2>
+            <ControlledCarousel></ControlledCarousel>
         </div>
     );
 }
