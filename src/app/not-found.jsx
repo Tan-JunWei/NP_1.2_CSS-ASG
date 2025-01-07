407295fb2359acf0daf6c20e33fb32239ec1c0aa
jwtan <jwtan0402@gmail.com>
2025-01-07 13:31:23 +0800

    fix: fix 404 not found page error

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,14 @@
+'use client'; 
+
 import React from "react";
 
 export default function Test404() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1 style={{ fontSize: "3rem", color: "red" }}>404 - Page Not Found</h1>
-      <p style={{ fontSize: "1.5rem" }}>Oops! The page you're looking for doesn't exist.</p>
+      <p style={{ fontSize: "1.5rem" }}>
+        Oops! The page you're looking for doesn't exist.
+      </p>
       <button 
         style={{ 
           marginTop: "20px", 
