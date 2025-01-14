36f57201a0a6c388f0d96153f1ddee9b2a653934
npskibidisigma <s10267231@connect.np.edu.sg>
2025-01-14 14:46:57 +0800

    chore: update history page css

--- src/app/history/page.jsx
+++ src/app/history/page.jsx
@@ -11,35 +11,35 @@ export default function History() {
                 <li>Evidence of early cooking (e.g., roasting meat over fire) dates back approximately 1.5 million years.</li>
                 <li>The transition from nomadic to settled life marked the domestication of animals and plants.</li>
             </ul>
-            <h2>~3000 BCE–500 CE</h2>
+            <h2 className={styles.subheader}>~3000 BCE–500 CE</h2>
             <ul>
                 <li>Humans of ancient civilizations developed food further, with bread and beer made using wheat and barley becoming common.</li>
                 <li>The development of rice in Asia, along with spices and sauces, became an integral part of food culture.</li>
             </ul>
-            <h2>~500–1500 CE</h2>
+            <h2 className={styles.subheader}>~500–1500 CE</h2>
             <ul>
                 <li>During the Medieval period, many countries developed unique food traditions.</li>
                 <li>Trade routes led to the discovery of new types of food.</li>
             </ul>
-            <h2>~1500–1800 CE</h2>
+            <h2 className={styles.subheader}>~1500–1800 CE</h2>
             <ul>
                 <li>Known as the Age of Exploration, global spice trades formed around the world.</li>
                 <li>Large transfers of food occurred between the Old and New Worlds.</li>
                 <li>New cooking methods, such as baking and roasting, were developed.</li>
             </ul>
-            <h2>19th Century</h2>
+            <h2 className={styles.subheader}>19th Century</h2>
             <ul>
                 <li>The Industrial Revolution brought advances in agriculture, such as mechanized plows and fertilizers, boosting food production.</li>
                 <li>Canned foods and pasteurization improved food storage.</li>
                 <li>Coffee, tea, and sugar became everyday staples worldwide.</li>
             </ul>
-            <h2>20th Century</h2>
+            <h2 className={styles.subheader}>20th Century</h2>
             <ul>
                 <li>This period saw globalization and innovation in food, with the development of processed foods like frozen, pre-packaged, and convenience foods.</li>
                 <li>The World Wars accelerated the development of canned food and MREs.</li>
                 <li>The Green Revolution introduced high-yield crops and modern farming techniques.</li>
             </ul>
-            <h2>21st Century</h2>
+            <h2 className={styles.subheader}>21st Century</h2>
             <ul>
                 <li>Focus on food sustainability, such as organic farming and reduction of food waste.</li>
                 <li>Development of global fusion cuisine as technology and transport blend traditional and modern cultures.</li>
