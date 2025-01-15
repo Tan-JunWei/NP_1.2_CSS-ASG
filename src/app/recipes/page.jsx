0e04e6eb5fc41bd2bb15f4dcb3e5b0b59e6f01f3
jwtan <jwtan0402@gmail.com>
2025-01-15 19:16:45 +0800

    feat: implement dropdown menu for filtering categories

--- src/app/recipes/page.jsx
+++ src/app/recipes/page.jsx
@@ -1,15 +1,44 @@
 /* Responsive Recipe page done by: Tan Jun Wei */
 
+"use client";
+
 import React from "react";
+import { useState } from "react";
 import recipes from "@/src/data/recipes"; // Import the recipes data
 import styles from "@/src/styles/pages/recipes.module.css";
 
 export default function Recipes() {
+  const [selectedTag, setSelectedTag] = useState("All");
+
+  const filteredRecipes = 
+    selectedTag === "All"
+      ? recipes
+      : recipes.filter((recipe) => recipe.tags.includes(selectedTag));
+
   return (
     <div className={styles.container}>
       <h1>Recipe List</h1>
+
+      <select
+        value={selectedTag}
+        onChange={(e) => setSelectedTag(e.target.value)}
+      >
+        <option value="All">All</option>
+        <option value="Hard">Hard</option>
+        <option value="Medium">Medium</option>
+        <option value="Easy">Easy</option>
+        <option value="Hawker Favourites">Hawker Favourites</option>
+        <option value="Chinese">Chinese</option>
+        <option value="Malaysian">Malaysian</option>
+        <option value="Indian">Indian</option>
+        <option value="Peranakan">Peranakan</option>
+        <option value="Teochew">Teochew</option>
+        <option value="Seafood">Seafood</option>
+        <option value="Spicy">Spicy</option>
+      </select>
+
       <div>
-        {recipes.map((recipe) => (
+        {filteredRecipes.map((recipe) => (
           <div key={recipe.id} className={styles.recipeCard}>
             <div className={styles.recipeText}>
               <div className={styles.recipeDescription}>
