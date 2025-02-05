4889fc9c957b70f321bb7196eb4b560567351bbc
Ryan Tan <jjryantan@gmail.com>
2025-02-05 17:04:11 +0800

    fix: add hero section to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React from "react";
 import styles from "./page.module.css";
-import RecipeGrid from "../components/recipegird/recipegird";
+import RecipeGrid from "../components/recipegrid/recipegrid";
 
 
 export default function Home() {
@@ -17,6 +17,5 @@ export default function Home() {
           <RecipeGrid></RecipeGrid>
         </div>
       </div>
-      
   );
 }
