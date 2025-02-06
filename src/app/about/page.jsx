f6c73474f830be1ddd8949c320650b0335c18f2a
Ryan Tan <jjryantan@gmail.com>
2025-02-06 17:20:39 +0800

    feat: center about page, add typing animation, replace placeholders

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,32 +1,41 @@
 /* About page done by: Ryan Tan Jia Jun */
-
+'use client';
 import Carousel from '@/src/components/image carousel/about_carousel';
 import Image from 'next/image';
-// import { StaticCopyUsage } from 'three';
 import styles from '@/src/styles/pages/about.module.css'
+import TypingAni from '@/src/components/typing animation/typingAni';
 
 export default function About() {
     return (
         <div>
-            <h1 className={styles.header}>About</h1>
             <p className={styles.main}>
             Discover the Flavors of Singapore!
             </p>
+            <div className={styles.Image}>
             <Image 
             id={styles.mainimage}
-            src="/about/Placeholder.png"
-            width={200}
-            height={150}
-            alt="PLACEHOLDER"/>
-            <p className={styles.p}>
+            src="/website logo.png"
+            width={250}
+            height={250}
+            alt="website logo"/>
+            </div>
+            <div className={styles.typing}>
+            <TypingAni></TypingAni>
+            </div>
+            <div className={styles.text}>
+            <p>
             Welcome to Plateful, a food blog dedicated to celebrating the joy of cooking, eating, and exploring diverse cuisines. 
             Whether you're a seasoned chef or a kitchen newbie, you'll find inspiration in our collection of flavorful recipes, cooking tips, and food stories from around the world. 
             Join us as we share the art of creating delicious dishes and the memories they inspire. 
             Let’s make every meal an adventure!
             </p>
-
+            </div>
+          
             <h2 className={styles.main}>Meet the team!</h2>
+            <div className={styles.Carousel}>
             <Carousel></Carousel>
+            </div>
+
         </div>
     );
 }
