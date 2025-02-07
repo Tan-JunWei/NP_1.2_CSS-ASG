5d722bc2c993255527215de8fe16cd5c118fd7d5
jwtan <jwtan0402@gmail.com>
2025-02-07 22:48:34 +0800

    feat: implement subtle Parallax effect for Hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,19 @@ import React from "react";
 import styles from "./page.module.css";
 import RecipeGrid from "@/src/components/recipegrid/recipegrid";
 import FAQ from "@/src/components/FAQ/faq";
+import SimpleParallax from "simple-parallax-js";
+
+const ParallaxHeroSection = () => (
+  <SimpleParallax orientation="up" scale={1.4}>
+    <img src="/singapore.jpg" alt="image" className={styles.parallaxImage} />
+  </SimpleParallax>
+);
 
 export default function Home() {
   return (
       <div className={styles.container}>
         <div className={styles.hero}>
+          <ParallaxHeroSection></ParallaxHeroSection>
           <h1 className={styles.header}>Welcome to Singapore's Food Paradise</h1>
           <p className={styles.subheader}>Discover the rich flavors and diverse culinary heritage of Singapore</p>
         </div>
