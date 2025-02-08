9161871774bdaf3c9fbe5170bd61e9727b6c3f6e
jwtan <jwtan0402@gmail.com>
2025-02-08 12:28:00 +0800

    feat: add scroll button for hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 import RecipeGrid from "@/src/components/recipegrid/recipegrid";
 import FAQ from "@/src/components/FAQ/faq";
 import SimpleParallax from "simple-parallax-js";
+import { FaChevronDown } from "react-icons/fa";
 
 const ParallaxHeroSection = () => (
   <SimpleParallax orientation="up" scale={1.4}>
@@ -18,9 +19,25 @@ export default function Home() {
           <ParallaxHeroSection></ParallaxHeroSection>
           <h1 className={styles.header}>Welcome to Singapore's Food Paradise</h1>
           <p className={styles.subheader}>Discover the rich flavors and diverse culinary heritage of Singapore</p>
+
+          <div className={styles.buttonWrapper}> {/* New wrapper for better positioning */}
+            <button 
+              className={styles.scrollButton} 
+              onClick={() => {
+                const targetElement = document.getElementById('contentSection');
+                window.scrollTo({
+                  top: targetElement.offsetTop - 70, // Offset by 70px to account for the fixed navbar
+                  behavior: 'smooth',
+                });
+              }}              
+            >
+              Explore
+              <FaChevronDown className={styles.bounceIcon}/>
+            </button>
+          </div>
         </div>
 
-        <div className={styles.content}>
+        <div className={styles.content} id="contentSection">
           <h2 className={styles.rectitle}>Popular Recipes</h2>
           <hr className={styles.line}/>
           <RecipeGrid></RecipeGrid>
